264e19d97422c5a354c115e62fb824c639aed3a1
tteckster <tteckster@gmail.com>
2022-02-05 21:30:27 -0500

    Update debian11_container.sh

--- ct/debian11_container.sh
+++ ct/debian11_container.sh
@@ -142,7 +142,7 @@ fi
 ARCH=$(dpkg --print-architecture)
 HOSTNAME=debian11
 TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
-pct create $CTID … -unprivileged 1 $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
+pct create $CTID -unprivileged 1 $TEMPLATE_STRING -arch $ARCH -features nesting=1 \
   -hostname $HOSTNAME -net0 name=eth0,bridge=vmbr0,ip=dhcp -onboot 1 -cores 1 -memory 512\
   -ostype $OSTYPE -rootfs $ROOTFS,size=$DISK_SIZE -storage $STORAGE >/dev/null
 
